0ece16d8f7bdebddcb2d39229da4b07ae861f15e
Stach Zagorski <stachzagorski@Stachs-MacBook-Pro.local>
2018-10-24 15:17:01 -0500

    merge conflict process

--- js/main.js
+++ js/main.js
@@ -44,6 +44,10 @@ function applyFilter(filterName){
 
     }
 
+    } else if(filterName=="bayer"){
+
+        bayer()
+    }
 }
 
 // _.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~
@@ -53,14 +57,25 @@ function applyFilter(filterName){
 function preload() {
     loadStarImage()
     loadCatImages()
+    loadMp3Sounds()
+    loadCowboyImages()
+    loadacidhouseImages()
 }
 
 function setup(){
-    createCanvas(innerWidth,innerHeight)
+    let canvas = createCanvas(innerWidth,innerHeight)
+    eraseSetUp(canvas.drawingContext)
+    setupNoiseBrush();
+}
+
+function mouseReleased(){
+    cowboyEnd(); //when mouse is released the audio stops playing
 }
 
 function draw(){
     if(mouseIsPressed){
+
+        toggleNoiseBrushUI(currentBrush);
 
         if(currentBrush=="basicPen"){
 
@@ -78,7 +93,29 @@ function draw(){
 
             catsBrush( mouseX, mouseY )
 
-        }
+        } else if(currentBrush=="cowboyBrush"){
 
+            cowboyBrush( mouseX, mouseY )
+
+        } else if(currentBrush=="noiseBrush"){
+
+            noiseBrush( mouseX, mouseY )
+
+        } else if(currentBrush=="erase"){
+
+            erase( mouseX , mouseY)
+
+        } else if (currentBrush=="djScrewBrush"){
+
+            djScrewBrush( mouseX, mouseY )
+
+        } else if(currentBrush=="acidhouseBrush"){
+
+            acidhouseBrush( mouseX, mouseY )
+
+        } else if(currentBrush== "textBrush"){
+
+             textBrush( mouseX, mouseY )
+        }
     }
 }
