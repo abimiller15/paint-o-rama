193a608919552665e6e7c829355b70a48ae31d21
sheeptipper <mzhang527@gmail.com>
2018-10-24 14:31:47 -0500

    cleaned up

--- js/brushes/textBrush.js
+++ js/brushes/textBrush.js
@@ -8,24 +8,25 @@ function textBrush(x,y){
     let indext = random(words.length)// pick a random index from words array
     indext = floor(indext)
 
-    background(50);
-    strokeWeight(2);//stroke() function is to set the color of the text’s outline.
-    fill(128 + sin(frameCount*0.1) * 128);// The color of the text is controlled with the fill() function
+    background(50)
+    strokeWeight(2)//stroke() function is to set the color of the text’s outline.
+    fill(128 + sin(frameCount*0.1) * 128)// The color of the text is controlled with the fill() function
 
     if (mouseIsPressed) {
-       noStroke(255);
-   } else { stroke(50);
+       noStroke(255)
+    } else { 
+       stroke(50)
     }
 
-    textLeading((mouseX / width) * 64);// sets the number of pixels to display between each line
+    textLeading((mouseX / width) * 64)// sets the number of pixels to display between each line
     textFont("Courier", 30)
-    textSize(12 + (mouseX / width)*72);
+    textSize(12 + (mouseX / width)*72)
 
 
 
     for (var i = 0; i < words.length; i++) {
-    fill(random(255));
-    text(words[i], random(width), random(height));
+        fill(random(255))
+        text(words[i], random(width), random(height))
     }
 
 
