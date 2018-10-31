f3a0f98ca6ddab73609306551523859906a32f97
Abi Miller <abimiller15@gmail.com>
2018-10-31 09:56:00 -0500

    clean-up

--- js/brushes/soundBrush.js
+++ js/brushes/soundBrush.js
@@ -8,30 +8,30 @@ let rotation = 3
 function loadSoundBrush(){
 	img = loadImage("images/catball.png") //add to main.js ???
 	musicTrack = loadSound("audio/catsong.mp3")
-	musicTrack.jump(10)
 }
 
 
 function setupSoundBrush() { //get rid of and add to main.js??
-	// start the music
 	amplitude = new p5.Amplitude()
+
 
 
 }
 
 function musicEnd(){
+	musicTrack.jump(15)
     musicTrack.stop()
 }
 
 function soundBrush(x,y) {
 		// getting the volume of the track
 		let level = amplitude.getLevel() // between 0 and 1
-		let c = map(level, 0, 1, 10, 400)
-		let radius = map(level, 0, 1, 10, 400)//how the sound is affecting the size
+		let c = map(level, 0, 1, 20, 400)
+		let radius = map(level, 0, 1, 20, 400)//how the sound is affecting the size
 
 
 		// catball version
-		rotation += .5 //better way to control this
+		rotation += .45 //better way to control this
 		push() //save
 			translate(mouseX, mouseY)
 			rotate(rotation)
